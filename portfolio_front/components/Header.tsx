264554e0b586c0bd772b533496929a9db7398946
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-07-25 12:51:00 +0100

    fixed bug where header didn't have access to window

--- portfolio_front/components/Header.tsx
+++ portfolio_front/components/Header.tsx
@@ -14,14 +14,17 @@ export const Header = ({ headTitle, prevImage }: HeaderProps) => {
         ? defaultImage
         : prevImage
 
-    return (
-        <Head>
-            <title>{pageTitle}</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <meta property="og:title" content={pageTitle} key="title" />
-            <meta property="og:type" content="website" />
-            <meta property="og:image" content={previewImage.src} />
-            <meta property="og:url" content={window.location.href} />
-        </Head>
-    )
+    if (process.browser) {
+        return (
+            <Head>
+                <title>{pageTitle}</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta property="og:title" content={pageTitle} key="title" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content={previewImage.src} />
+                <meta property="og:url" content={window.location.href} />
+            </Head>
+        )
+    }
+    else return <Head><title>{pageTitle}</title></Head>
 }
