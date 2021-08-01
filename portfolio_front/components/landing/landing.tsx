3487ac61b47afd1d7dbe9cd4d3203fae365e439f
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-08-01 23:07:10 +0100

    finished landing, also added some helper components

--- portfolio_front/components/landing/landing.tsx
+++ portfolio_front/components/landing/landing.tsx
@@ -1,3 +1,12 @@
+import { Section } from "../section"
+import styles from './landing.module.scss'
+
 export const Landing = () => {
-    
+    return (
+        <Section sectionName="landing" className={styles.landing}>
+            <h1>RAVEN</h1>
+            <h1>DUFFY</h1>
+            <h2>Web Developer</h2>
+        </Section>
+    )
 }
