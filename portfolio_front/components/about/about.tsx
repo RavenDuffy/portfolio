b4709f8c5027902832a1621fe2ed54d572faea96
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-08-02 22:00:06 +0100

    updated global styles, fixed issue with mobile nav, fixed issue with mobile landing

--- portfolio_front/components/about/about.tsx
+++ portfolio_front/components/about/about.tsx
@@ -1,13 +1,14 @@
 import { DescBox } from "../desc-box"
 import { Section } from "../section"
+import styles from './about.module.scss'
 
 export const About = () => {
 
 
     return (
         <Section sectionName="about" id="about" isWidthCapped>
-            <h1>Hi I’m Raven, a <span className="highlightText">fullstack web developer</span>.</h1>
-            <p>I’ve been working with web tech stacks for years both commercially and personally. 
+            <h1 className={styles.header}>Hi I’m Raven, a <span className="highlightText">fullstack web developer</span>.</h1>
+            <p className={styles.blurb}>I’ve been working with web tech stacks for years both commercially and personally. 
                 I love working within the constantly evolving world of web dev because I can 
                 always learn something new. I’m currently working at Sycous but I’m happy to get 
                 in contact for small projects.</p>
