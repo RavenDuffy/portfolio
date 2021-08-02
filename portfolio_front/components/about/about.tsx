1e615cf767efabebb931358318f867a91dfa406a
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-08-02 21:41:02 +0100

    rejigged nav, added scroll util and applied it to the find out more button

--- portfolio_front/components/about/about.tsx
+++ portfolio_front/components/about/about.tsx
@@ -5,7 +5,7 @@ export const About = () => {
 
 
     return (
-        <Section sectionName="about" isWidthCapped>
+        <Section sectionName="about" id="about" isWidthCapped>
             <h1>Hi I’m Raven, a <span className="highlightText">fullstack web developer</span>.</h1>
             <p>I’ve been working with web tech stacks for years both commercially and personally. 
                 I love working within the constantly evolving world of web dev because I can 
