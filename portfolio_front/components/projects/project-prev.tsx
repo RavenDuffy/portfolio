a304eada821400024035c501dc65a5cc8c3d018c
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-08-06 00:57:37 +0100

    moved a tag to surround project preview

--- portfolio_front/components/projects/project-prev.tsx
+++ portfolio_front/components/projects/project-prev.tsx
@@ -6,23 +6,30 @@ interface ProjectPreviewProps {
 
 export const ProjectPreview = ({ name, image, link }: ProjectPreviewProps) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url('${image}')`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        width: '100%',
-        height: '100%',
-        borderRadius: '10px',
-        position: 'relative',
-      }}
-    >
-      <a
-        href={link}
-        style={{ position: 'absolute', bottom: '2rem', right: '2rem' }}
+    <a href={link}>
+      <div
+        style={{
+          backgroundImage: `url('${image}')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          width: '100%',
+          height: '100%',
+          borderRadius: '10px',
+          position: 'relative',
+          cursor: 'pointer',
+        }}
       >
-        {name}
-      </a>
-    </div>
+        <p
+          style={{
+            position: 'absolute',
+            bottom: '1.5rem',
+            right: '2rem',
+            textShadow: '1px 1px 5px #000f',
+          }}
+        >
+          {name}
+        </p>
+      </div>
+    </a>
   )
 }
