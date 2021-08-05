8250318c3bb9a214425f9a7ec83609850968c7c6
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-08-06 00:50:06 +0100

    retouched ProjectPreview

--- portfolio_front/components/projects/project-prev.tsx
+++ portfolio_front/components/projects/project-prev.tsx
@@ -1,22 +1,28 @@
 interface ProjectPreviewProps {
   name: string
-  image?: any
-  style?: React.CSSProperties
+  image?: string
+  link?: string
 }
 
-export const ProjectPreview = ({ name, image, style }: ProjectPreviewProps) => {
+export const ProjectPreview = ({ name, image, link }: ProjectPreviewProps) => {
   return (
     <div
       style={{
-        backgroundImage: `url('${image.src}')`,
+        backgroundImage: `url('${image}')`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
         width: '100%',
         height: '100%',
         borderRadius: '10px',
+        position: 'relative',
       }}
     >
-      <p>{name}</p>
+      <a
+        href={link}
+        style={{ position: 'absolute', bottom: '2rem', right: '2rem' }}
+      >
+        {name}
+      </a>
     </div>
   )
 }
