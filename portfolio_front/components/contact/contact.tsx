d8973dc0b6b714d0fb051d92f1a105a075512762
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-08-08 00:51:02 +0100

    fixed some nav issues, added tooltip poc

--- portfolio_front/components/contact/contact.tsx
+++ portfolio_front/components/contact/contact.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { RefObject, useEffect, useRef, useState } from 'react'
 import { Section } from '../section'
 import styles from './contact.module.scss'
+import ReactTooltip from 'react-tooltip'
 
 interface FormData {
   name?: string
@@ -8,12 +9,18 @@ interface FormData {
   details?: string
 }
 
-export const ContactMe = () => {
-  const handleSubmit = (event: any) => {
-    event.preventDefault()
-    console.log(formData)
-  }
+const isEmpty = (field: string | undefined): boolean => {
+  return field === undefined || field === ''
+}
 
+const isValidEmail = (email: string | undefined) => {
+  if (email === undefined) return false
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(email)
+}
+
+export const ContactMe = () => {
   const handleChange = (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault()
     setFormData({
@@ -22,7 +29,29 @@ export const ContactMe = () => {
     })
   }
 
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+    if (!isEmpty(formData?.name)) {
+    }
+    if (!isEmpty(formData?.email) && isValidEmail(formData?.email)) {
+    }
+    if (!isEmpty(formData?.details)) {
+    }
+    console.log(
+      formData,
+      !isEmpty(formData?.email) && isValidEmail(formData?.email)
+    )
+    ReactTooltip.show(nameRef.current)
+  }
+
   const [formData, setFormData] = useState<FormData>()
+  const [isTooltipMounted, setTooltipMounted] = useState<boolean>(false)
+  const nameRef = useRef<any>(null)
+
+  useEffect(() => {
+    setTooltipMounted(true)
+    ReactTooltip.rebuild()
+  }, [])
 
   return (
     <Section
@@ -46,7 +75,23 @@ export const ContactMe = () => {
       >
         <div className={styles.inputLabelWrapper}>
           <label htmlFor='name'>Name</label>
-          <input id='name' placeholder='my name is...'></input>
+          <input
+            id='name'
+            data-tip='test'
+            data-for='nameTip'
+            placeholder='my name is...'
+            ref={nameRef as RefObject<HTMLInputElement>}
+          ></input>
+          {isTooltipMounted && (
+            <ReactTooltip
+              id='nameTip'
+              place='bottom'
+              effect='solid'
+              type='error'
+              offset={{ top: 4 }}
+              event={'none'}
+            />
+          )}
         </div>
         <div className={styles.inputLabelWrapper}>
           <label>Email</label>
