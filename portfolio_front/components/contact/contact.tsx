3a0a10979b51e5f66435f0da6265c31d5953abd2
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-08-07 22:03:50 +0100

    added onChange and tweaked onSubmit handlers:

--- portfolio_front/components/contact/contact.tsx
+++ portfolio_front/components/contact/contact.tsx
@@ -1,11 +1,28 @@
+import React, { useState } from 'react'
 import { Section } from '../section'
 import styles from './contact.module.scss'
+
+interface FormData {
+  name?: string
+  email?: string
+  details?: string
+}
 
 export const ContactMe = () => {
   const handleSubmit = (event: any) => {
     event.preventDefault()
-    console.log(event.target)
+    console.log(formData)
   }
+
+  const handleChange = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    })
+  }
+
+  const [formData, setFormData] = useState<FormData>()
 
   return (
     <Section
@@ -22,18 +39,25 @@ export const ContactMe = () => {
       <h1 className={styles.contactTitle}>
         Like what you see? Lets get in contact!
       </h1>
-      <form className={styles.formWrapper} onSubmit={handleSubmit}>
+      <form
+        className={styles.formWrapper}
+        onSubmit={handleSubmit}
+        onChange={handleChange}
+      >
         <div className={styles.inputLabelWrapper}>
           <label htmlFor='name'>Name</label>
-          <input id='name'></input>
+          <input id='name' placeholder='my name is...'></input>
         </div>
         <div className={styles.inputLabelWrapper}>
           <label>Email</label>
-          <input id='email'></input>
+          <input id='email' placeholder='my email is...'></input>
         </div>
         <div className={`${styles.inputLabelWrapper} ${styles.spanTwo}`}>
           <label>Project Details</label>
-          <textarea id='details'></textarea>
+          <textarea
+            id='details'
+            placeholder='my project is about...'
+          ></textarea>
         </div>
         <button type='submit' className={`${styles.submit} ${styles.spanTwo}`}>
           Get in Touch
