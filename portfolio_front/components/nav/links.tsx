b41ea6fa121381354b68fcbf98f4d8fcc1dddcf0
RavenDuffy <40807825+RavenDuffy@users.noreply.github.com>
2021-08-07 13:28:34 +0100

    closing mobile nav on click

--- portfolio_front/components/nav/links.tsx
+++ portfolio_front/components/nav/links.tsx
@@ -4,30 +4,57 @@ import styles from './nav.module.scss'
 interface NavLinksProps {
   isMobile?: boolean
   isHamburgerOpen?: boolean
+  setIsOpen?: (isOpen: boolean) => void
 }
 
-const Links = () => {
+const Links = ({ isHamburgerOpen, setIsOpen }: NavLinksProps) => {
   return (
     <>
-      <button onClick={() => scrollToElement('about')}>
+      <button
+        onClick={() => {
+          scrollToElement('about')
+          setIsOpen && setIsOpen(!isHamburgerOpen)
+        }}
+      >
         <h2>About</h2>
       </button>
-      <button onClick={() => scrollToElement('projects')}>
+      <button
+        onClick={() => {
+          scrollToElement('projects')
+          setIsOpen && setIsOpen(!isHamburgerOpen)
+        }}
+      >
         <h2>Projects</h2>
       </button>
-      <button onClick={() => scrollToElement('')}>
+      <button
+        onClick={() => {
+          scrollToElement('')
+          setIsOpen && setIsOpen(!isHamburgerOpen)
+        }}
+      >
         <h2>Contact</h2>
       </button>
     </>
   )
 }
 
-export const NavLinks = ({ isMobile, isHamburgerOpen }: NavLinksProps) => {
+export const NavLinks = ({
+  isMobile,
+  isHamburgerOpen,
+  setIsOpen,
+}: NavLinksProps) => {
   if (!isMobile)
     return (
       <div className={styles.links}>
         <Links />
       </div>
     )
-  else return <>{isHamburgerOpen && <Links />}</>
+  else
+    return (
+      <>
+        {isHamburgerOpen && (
+          <Links isHamburgerOpen={isHamburgerOpen} setIsOpen={setIsOpen} />
+        )}
+      </>
+    )
 }
